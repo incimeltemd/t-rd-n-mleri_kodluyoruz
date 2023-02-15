c74a44abaad84a108cc1c356e33d87098a6339c8
incimeltemd <incimeltemdaglaroglu@gmail.com>
2023-02-15 15:14:18 +0300

    ödev eklendi

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿////
+
+//implicit tür dönüşümleri
+byte a = 5;
+int b = a;
+
+string c = "meltem";
+object d = c;
+
+//explicit tür dönüşümleri
+int e = 45;
+byte f = (byte)e;
+
+string g = e.ToString();
+
+string h = "6";
+int j = Convert.ToInt32(h);
+
+string k = "27";
+int l = Int32.Parse(k);
